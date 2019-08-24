a7336eb3302d34026367d0d23c02ac40977102d5
SafeWinter <zandong_19@aliyun.com>
2019-08-24 19:51:07 +0800

    Rename the ts file with a uniform pattern.

--- ts_in_action/src/index.ts
+++ ts_in_action/src/index.ts
@@ -1,11 +1,11 @@
-// import "./datatype"     /* handout for Lesson 5 */
-// import "./enum"         /* handout for Lesson 6 */
-// import "./interface"    /* handout for Lesson 7 */
-// import "./interface_L8" /* handout for Lesson 8 */
-// import "./function"     /* handout for Lesson 9 */
-// import "./class_L10"    /* handout for Lesson 10 */
-// import "./class_L11"    /* handout for Lesson 11 */
-// import "./class-interface_L12"    /* handout for Lesson 12 */
+// import "./L05_datatype"          /* handout for Lesson 5 */
+// import "./L06_enum"              /* handout for Lesson 6 */
+// import "./L07_interface"         /* handout for Lesson 7 */
+// import "./L08_interface"         /* handout for Lesson 8 */
+// import "./L09_function"          /* handout for Lesson 9 */
+// import "./L10_class"             /* handout for Lesson 10 */
+// import "./L11_class"             /* handout for Lesson 11 */
+// import "./L12_class-interface"   /* handout for Lesson 12 */
 
 let hello : string = "Lesson 12 - Class vs Interface";
 document.querySelectorAll(".app")[0].innerHTML = hello;
